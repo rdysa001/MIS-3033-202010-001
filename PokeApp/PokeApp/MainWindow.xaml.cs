9c3c9118c8ddd19a2bdaaa9b92a53a4b27bcbc42
Reid Dysart <reid.dysart@ou.edu>
2020-09-29 18:13:49 -0500

    Almost Done!

---
+++ PokeApp/PokeApp/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+
+namespace PokeApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string url = @"https://pokeapi.co/api/v2/pokemon?offset=0&limit=1100";
+            PokemonNameAPI api;
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(url).Result;
+
+                api = JsonConvert.DeserializeObject<PokemonNameAPI>(json);
+
+            }
+
+            foreach (var name in api.results)
+            {
+                lstPoke.Items.Add(name);
+            }
+        }
+
+        private void btnGetInfo_Click(object sender, RoutedEventArgs e)
+        {
+            Monster selectedMonster = (Monster)lstPoke.SelectedItem;
+            
+
+            string selectedUrl = selectedMonster.url;
+            info infoAPI;
+            using (var client2 = new HttpClient())
+            {
+                string infoSelected = client2.GetStringAsync(selectedUrl).Result;
+                infoAPI = JsonConvert.DeserializeObject<info>(infoSelected);
+            }
+            txtHeight.Text = infoAPI.height.ToString();
+            txtWeight.Text = infoAPI.weight.ToString();
+
+            
+            Uri uriFront = new Uri(infoAPI.front_default);
+            Uri uriBack = new Uri(infoAPI.back_default);
+            BitmapImage pictureFront = new BitmapImage(uriFront);
+            BitmapImage pictureBack = new BitmapImage(uriBack);
+
+            imgSprite.Source = pictureFront;
+        }
+    }
+}
---
+++ PokeApp/PokeApp/PokemonNameAPI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeApp
+{
+    class PokemonNameAPI
+    {
+        public List<Monster> results { get; set; }
+        public info info { get; set; }
+    }
+    public class Monster
+    {
+
+        public string name { get; set; }
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+    public class info
+    {
+        public string front_default { get; set; }
+        public string back_default { get; set; }
+        public int height { get; set; }
+        public int weight { get; set; }
+    }
+}
