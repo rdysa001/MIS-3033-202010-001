7f2a4b5d288f6302db0a6d58ce84a70747cb4364
Reid Dysart <reid.dysart@ou.edu>
2020-10-01 18:15:59 -0500

    Almost there? again?

--- PokeApp/PokeApp/MainWindow.xaml.cs
+++ PokeApp/PokeApp/MainWindow.xaml.cs
@@ -23,14 +23,19 @@ namespace PokeApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        private info infoAPI;
+        
+        //public imgSprite.Source = null;
+        //public BitmapImage pictureFront = new BitmapImage();
+        //public BitmapImage pictureBack = new BitmapImage();
+
 
         public MainWindow()
         {
             InitializeComponent();
+            imgSprite.Source = null;
+
             string url = @"https://pokeapi.co/api/v2/pokemon?offset=0&limit=1100";
             PokemonNameAPI api;
-            info infoAPI;
             using (var client = new HttpClient())
             {
                 string json = client.GetStringAsync(url).Result;
@@ -44,6 +49,9 @@ namespace PokeApp
                 lstPoke.Items.Add(name);
             }
         }
+        
+        private info infoAPI;
+        public bool showFront = true;
 
         private void btnGetInfo_Click(object sender, RoutedEventArgs e)
         {
@@ -56,7 +64,6 @@ namespace PokeApp
                 Monster selectedMonster = (Monster)lstPoke.SelectedItem;
 
                 string selectedUrl = selectedMonster.url;
-                //info infoAPI;
                 using (var client2 = new HttpClient())
                 {
                     string infoSelected = client2.GetStringAsync(selectedUrl).Result;
@@ -68,19 +75,28 @@ namespace PokeApp
                 txtWeight.Text = infoAPI.weight.ToString();
 
 
-                Uri uriFront = new Uri(infoAPI.sprites.front_default));
+                Uri uriFront = new Uri(infoAPI.sprites.front_default);
                 Uri uriBack = new Uri(infoAPI.sprites.back_default);
                 BitmapImage pictureFront = new BitmapImage(uriFront);
                 BitmapImage pictureBack = new BitmapImage(uriBack);
-
+                
                 imgSprite.Source = pictureFront;
             }
             
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        private void Button_Click(object sender, RoutedEventArgs e, ImageSource pictureFront, ImageSource pictureBack)
         {
-            
+            if (showFront)
+            {
+                imgSprite.Source = pictureFront;
+            }
+            else
+            {
+                imgSprite.Source = pictureBack;
+            }
+            showFront = !showFront;
         }
     }
 }
