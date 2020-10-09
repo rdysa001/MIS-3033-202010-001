3a3575d4e65ca1a9ff43dc75ce2800633e9b59fb
Reid Dysart <reid.dysart@ou.edu>
2020-10-08 20:35:38 -0500

    Almost done with this thing.

---
+++ ChuckNorrisJoke/ChuckNorrisJoke/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Collections.Concurrent;
+
+namespace ChuckNorrisJoke
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string joke;
+        public MainWindow()
+        {
+            InitializeComponent();
+            string url = @"https://api.chucknorris.io/jokes/categories";
+            List<string> categories = new List<string>();
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(url).Result;
+
+                categories = JsonConvert.DeserializeObject<List<string>>(json);
+            }
+            categories.Insert(0, "All");
+            foreach (var item in categories)
+            {
+                cmboCategory.Items.Add(item);
+            }
+        }
+        private void btnGetJoke_Click(object sender, RoutedEventArgs e)
+        {
+            if (cmboCategory.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a category.");
+            }
+            else if (cmboCategory.SelectedItem.ToString() == "All")
+            {
+                joke = @"https://api.chucknorris.io/jokes/random";
+            }
+            else
+            {
+                string category = cmboCategory.Text;
+                joke = @"https://api.chucknorris.io/jokes/random?category=" + category;
+            }
+        }
+    }
+}
--- PokeApp/PokeApp/MainWindow.xaml.cs
+++ PokeApp/PokeApp/MainWindow.xaml.cs
@@ -23,17 +23,10 @@ namespace PokeApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        
-        //public imgSprite.Source = null;
-        //public BitmapImage pictureFront = new BitmapImage();
-        //public BitmapImage pictureBack = new BitmapImage();
-
-
         public MainWindow()
         {
             InitializeComponent();
             imgSprite.Source = null;
-
             string url = @"https://pokeapi.co/api/v2/pokemon?offset=0&limit=1100";
             PokemonNameAPI api;
             using (var client = new HttpClient())
@@ -41,7 +34,6 @@ namespace PokeApp
                 string json = client.GetStringAsync(url).Result;
 
                 api = JsonConvert.DeserializeObject<PokemonNameAPI>(json);
-
             }
 
             foreach (var name in api.results)
@@ -49,46 +41,45 @@ namespace PokeApp
                 lstPoke.Items.Add(name);
             }
         }
-        
+
         private info infoAPI;
         public bool showFront = true;
 
         private void btnGetInfo_Click(object sender, RoutedEventArgs e)
         {
-            if (lstPoke.SelectedItem is null)
+            switch (lstPoke.SelectedItem)
             {
-                MessageBox.Show("Please select a Pokemon");
+                case null:
+                    MessageBox.Show("Please select a Pokemon");
+                    break;
+                default:
+                    Monster selectedMonster = (Monster)lstPoke.SelectedItem;
+
+                    string selectedUrl = selectedMonster.url;
+                    using (var client2 = new HttpClient())
+                    {
+                        string infoSelected = client2.GetStringAsync(selectedUrl).Result;
+                        infoAPI = JsonConvert.DeserializeObject<info>(infoSelected);
+                    }
+                    txtHeight.Text = infoAPI.height.ToString();
+                    txtWeight.Text = infoAPI.weight.ToString();
+
+                    Uri uriFront = new Uri(infoAPI.sprites.front_default);
+                    Uri uriBack = new Uri(infoAPI.sprites.back_default);
+                    BitmapImage pictureFront = new BitmapImage(uriFront);
+                    BitmapImage pictureBack = new BitmapImage(uriBack);
+                    imgSprite.Source = pictureFront;
+                    break;
             }
-            else
-            {
-                Monster selectedMonster = (Monster)lstPoke.SelectedItem;
-
-                string selectedUrl = selectedMonster.url;
-                using (var client2 = new HttpClient())
-                {
-                    string infoSelected = client2.GetStringAsync(selectedUrl).Result;
-                    infoAPI = JsonConvert.DeserializeObject<info>(infoSelected);
-
-
-                }
-                txtHeight.Text = infoAPI.height.ToString();
-                txtWeight.Text = infoAPI.weight.ToString();
-
-
-                Uri uriFront = new Uri(infoAPI.sprites.front_default);
-                Uri uriBack = new Uri(infoAPI.sprites.back_default);
-                BitmapImage pictureFront = new BitmapImage(uriFront);
-                BitmapImage pictureBack = new BitmapImage(uriBack);
-                
-                imgSprite.Source = pictureFront;
-            }
-            
         }
 
-
-        private void Button_Click(object sender, RoutedEventArgs e, ImageSource pictureFront, ImageSource pictureBack)
+        private void btnToggle_Click(object sender, RoutedEventArgs e)
         {
-            if (showFront)
+            Uri uriFront = new Uri(infoAPI.sprites.front_default);
+            Uri uriBack = new Uri(infoAPI.sprites.back_default);
+            BitmapImage pictureFront = new BitmapImage(uriFront);
+            BitmapImage pictureBack = new BitmapImage(uriBack);
+            if (!showFront)
             {
                 imgSprite.Source = pictureFront;
             }
